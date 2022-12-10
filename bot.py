47385f394a616711c134cdaee96d5d5948821546
Jahias17 <jahias17@gmail.com>
2022-12-10 00:04:30 -0500

    some planning of features

--- bot.py
+++ bot.py
@@ -1,28 +1,32 @@
 import discord
+import random
 import os
 from dotenv import load_dotenv
 
-# class MyClient(discord.Client):
-#     async def on_ready(self):
-#         print(f'Logged on as {self.user}!')
 
-#     async def on_message(self, message):
-#         print(f'Message from {message.author}: {message.content}')
-
-# intents = discord.Intents.default()
-# intents.message_content = True
-
-# client = MyClient(intents=intents)
-#load_dotenv()
-
-#TOKEN = os.getenv('DISCORD_TOKEN')
-
-#client = discord.Client()
 intents = discord.Intents(messages=True, guilds=True, typing=True)
 intents.message_content = True
-#intents = discord.Intents.all()
+
 client = discord.Client(intents=intents)
 
+
+# stats and positions, reroll stats?
+# simulated goals and stat lines
+# chance of goal every 2 minutes
+
+# shot, skating, stickhandling, passing, strength
+# shot: goal odds
+# skating: goal odds, breakaways?
+# stickhandling: goal odds, passing, turnovers
+# strength: turnovers
+# passing: teammate goal odds
+
+
+#write data to text file
+
+
+data = []
+data2 = {}
 
 @client.event
 async def on_ready():
@@ -33,7 +37,17 @@ async def on_message(message):
     if message.author == client.user:
         return
     print(message.content)
+    if message.author not in data:
+        data.append(message.author)
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+    if message.content.startswith('$shoot'):
+        score = random.random()
+        print(score)
+        if score > 0.7:
+            await message.channel.send('SCORED!!!')
+        else:
+            await message.channel.send('SAVED!!!')
+
 
 client.run("OTgxNzMxNDQ5NTI0ODQ2NTky.GgGwTE.CspOyoBrN6t01-zM-iqrnC66tRsULqJ8kaeSdE")
