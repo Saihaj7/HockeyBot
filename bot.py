aa53003a6457112f7e118853bc56d5061c9a796a
Jahias17 <jahias17@gmail.com>
2022-12-10 01:51:08 -0500

    working shot rating and reroll system as well as working fetch memberlist

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
 import discord
 import random
 import os
+from player import Player
 from dotenv import load_dotenv
 
 
 intents = discord.Intents(messages=True, guilds=True, typing=True)
 intents.message_content = True
+intents.members = True
+intents.guilds = True
 
 client = discord.Client(intents=intents)
 
@@ -25,29 +28,53 @@ client = discord.Client(intents=intents)
 #write data to text file
 
 
-data = []
-data2 = {}
+#data = []
+data = {}
+
+for user in client.users:
+        print(user.name)
+        if user not in data:
+            player = Player(user)
+            data['user'] = player
+
+
+
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    async for guild in client.fetch_guilds(limit=150):
+        if guild.name == "The Dog Pound":
+            async for member in guild.fetch_members(limit=150):
+                player = Player(member)
+                data[member] = player
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    print(message.content)
-    if message.author not in data:
-        data.append(message.author)
+
+    if message.content.startswith("$attribute"):
+        updated = "Your shot is rated {0} (on a scale from 1-20)".format(data[message.author].stats['shot'])
+        await message.channel.send(updated)
+
+    if message.content.startswith("$reroll"):
+        data[message.author].reroll()
+        updated = "Your shot is now {0}".format(data[message.author].stats['shot'])
+        await message.channel.send(updated)
+
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
     if message.content.startswith('$shoot'):
         score = random.random()
-        print(score)
         if score > 0.7:
             await message.channel.send('SCORED!!!')
         else:
             await message.channel.send('SAVED!!!')
+    #if message.content.startswith('$play'):
+        #await 
+
 
 
 client.run("OTgxNzMxNDQ5NTI0ODQ2NTky.GgGwTE.CspOyoBrN6t01-zM-iqrnC66tRsULqJ8kaeSdE")
--- player.py
+++ player.py
@@ -1,14 +1,18 @@
 import discord
-import bot
 import random
 import os
 from dotenv import load_dotenv
 
 class Player:
-    def __init__(self, name, stats):
-        self.name = name
+    def __init__(self, user):
+        self.user = user
         self.stats = {'shot': random.randint(1, 20)}
 
     def reroll(self):
-        self.stats['stats'] = random.randint(1, 20)
+        self.stats['shot'] = random.randint(1, 20)
 
+    def __repr__(self):
+        return "Player: {0}, with a shot attribute of {1}".format(self.user, self.stats['shot'])
+
+    def __str__(self):
+        return self.user.name
