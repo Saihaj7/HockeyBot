fa28f11a7dac3ac1be632902424e878a2abb4727
Jahias17 <jahias17@gmail.com>
2023-01-03 00:29:29 -0500

    playing around with more complex events

--- game.py
+++ game.py
@@ -26,7 +26,7 @@ class Game:
         self.team_names = random.sample(TEAMS, 2)
         if len(players) >= 12:
             self.team1 = players[:6]
-            self.team2 = players[6:]                    # I feel like this could be way better
+            self.team2 = players[6:]                    # I feel like this could be way better, Team class?
             self.goalie1 = self.data[self.team1[-1]]
             self.goalie2 = self.data[self.team2[-1]]
 
@@ -43,24 +43,32 @@ class Game:
             #await sleep(1)
             clock = await clock.edit(content="PERIOD {0}: {1}:00".format(1 + minute // 20, 20 - minute % 20)) #GAME TIME
             event = random.random()                                     
-            player1 = self.data[self.team1[random.randint(0, 4)]] #grabs Player object
-            player2 = self.data[self.team2[random.randint(0, 4)]] #grabs Player object
+            player1 = self.data[self.team1[random.randint(0, 4)]] #grabs Player object   #team.py change; will grab from skaters i.e
+            player2 = self.data[self.team2[random.randint(0, 4)]] #grabs Player object   # player1 = self.data[team.get_skaters()[random.randint(0, len(team.get_skaters) - 1)]]
             if event <= 0.85: #SHOT
                 if random.randint(1, 2) == 1: #Team1
                     self.shots[0] = self.shots[0] + 1
-                    player_xG = (player1.shot_val() / 10) * xG.rvs()
+                    player_xG = (player1.shot_val() / 10) * xG.rvs() # expected goal odds modified by shot value
                     if random.random() < player_xG: #GOAL
-                        msg = await msg.edit(content=msg.content+"\n:rotating_light:{2} Goal scored by {0} on {1}:rotating_light:".format(player1.user.display_name, self.goalie2.user.display_name, self.team_names[0]))
-                        self.score[0] = self.score[0] + 1
+                        if random.random() < 0.5:
+                            msg = await msg.edit(content=msg.content+"\n:rotating_light:{2} Goal scored by {0}, assisted by {1}, {3}:rotating_light:".format(player1.user.display_name, self.goalie2.user.display_name, self.team_names[0]))
+                            self.score[0] = self.score[0] + 1
+                        elif random.random() < 0.8:
+                            msg = await msg.edit(content=msg.content+"\n:rotating_light:{2} Goal scored by {0}, assisted by {1}:rotating_light:".format(player1.user.display_name, self.goalie2.user.display_name, self.team_names[0]))
+                            self.score[0] = self.score[0] + 1
+                        else: #UNASSISTED
+                            msg = await msg.edit(content=msg.content+"\n:rotating_light:{2} Goal scored by {0}:rotating_light:".format(player1.user.display_name, self.goalie2.user.display_name, self.team_names[0]))
+                            self.score[0] = self.score[0] + 1
                     
                 else:                           #Team2
                     self.shots[1] = self.shots[1] + 1
-                    player_xG = (player2.shot_val() / 10) * xG.rvs()
+                    player_xG = (player2.shot_val() / 10) * xG.rvs() # expected goal odds modified by shot value
                     if random.random() < player_xG: #GOAL
                         msg = await msg.edit(content=msg.content+"\n:rotating_light:{2} Goal scored by {0} on {1}:rotating_light:".format(player2.user.display_name, self.goalie1.user.display_name, self.team_names[1]))
                         self.score[1] = self.score[1] + 1
                     
                 score = await score.edit(content="-----SCORE-----\n{4}: {0} --- {5}: {1}\n-----SHOTS-----\n{4}: {2} --- {5}: {3}".format(self.score[0], self.score[1], self.shots[0], self.shots[1], self.team_names[0], self.team_names[1]))
+
             elif event <= 0.9: #HIT
                 if random.randint(1, 2) == 1: #Team1
                     msg = await msg.edit(content=msg.content+"\n{0} hit {1}".format(player1.user.display_name, player2.user.display_name))
@@ -80,3 +88,7 @@ class Game:
         clock = await clock.edit(content="PERIOD 3: 00:00")
         msg = await msg.edit(content=msg.content+"\n\nFinal Score: {0}-{1}".format(self.score[0], self.score[1]))
         msg = await msg.edit(content=msg.content+"\nShot Totals: {0}-{1}".format(self.shots[0], self.shots[1]))
+
+
+      # starting fraction < event < player1.fraction for hit/takeaway/block? 
+      # scaled by att
--- team.py
+++ team.py
@@ -9,14 +9,34 @@ class Team:
     def __init__(self, players, goalie):
         if len(players) == 6:
             self.players = players
-        self.state = 5 #players on the ice
+        self.state = 5 #players on the ice, don't forget to change event odds depending on game state, i.e. make shots unlikely for pk team, opposite for pp team
         self.goalie = goalie
+        self.skaters = players[:5] # self.players exluding those in penalty box and goalie
 
     def get_players(self):
         return self.players
+
+    def get_skaters(self):
+        return self.skaters
 
     def get_state(self):
         return self.state
 
     def get_goalie(self):
         return self.goalie
+
+    def penalty(self, player):
+        if player not in self.skating: #returns player 
+            self.skating.append(player)
+            self.state = self.state + 1
+        elif self.state >= 3:           #put player in the penalty box (i.e. makes them unavailable for event selection)
+            self.skating.remove(player)
+            self.state = self.state - 1
+
+    def goalie_pull(self):
+        self.skating.append(self.goalie)
+        self.state = self.state + 1
+    
+    def goalie_return(self):
+        self.skating.remove(self.goalie)
+        self.state = self.state - 1
